b9700fb32406ec7d1c77ad27857ef5520cf86a60
Joe Stella <jstella@fid-x.com>
2026-06-29 16:16:38 -0400

    Add Firebase config for live leaderboard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+  apiKey: "AIzaSyBpEpdVYrFvXTkZ9RHznQG1ssuezP2EVxg",
+  authDomain: "mlb-connections.firebaseapp.com",
+  projectId: "mlb-connections",
+  storageBucket: "mlb-connections.firebasestorage.app",
+  messagingSenderId: "682879107447",
+  appId: "1:682879107447:web:a0f4aeb79384b24c4ee172",
+  measurementId: "G-3JXSXVLYQN"
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+  apiKey: "AIzaSyBpEpdVYrFvXTkZ9RHznQG1ssuezP2EVxg",
+  authDomain: "mlb-connections.firebaseapp.com",
+  projectId: "mlb-connections",
+  storageBucket: "mlb-connections.firebasestorage.app",
+  messagingSenderId: "682879107447",
+  appId: "1:682879107447:web:382585fda46e46394ee172",
+  measurementId: "G-FV54WC7CV4"
   },
 };
